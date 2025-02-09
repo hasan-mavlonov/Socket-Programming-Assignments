25d6bf8635b557154d0be1dbb282d005713ea237
Hasan Mavlonov <hmavlanov79@gmail.com>
2025-02-09 17:57:33 +0500

    created a UDP server

---
+++ UDP Pinger.py
@@ -0,0 +1,15 @@
+from socket import *
+import random
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print('Started UDP server on port: ', {serverPort})
+while True:
+    rand = random.randint(0, 10)
+    message, address = serverSocket.recvfrom(1024)
+    message = message.decode()
+    message = message.upper()
+    if rand < 4:
+        continue
+    serverSocket.sendto(message.encode(), address)
